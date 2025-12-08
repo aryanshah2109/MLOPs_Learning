e0913ea03b378114a76baf67a3b047074be87f3d
aryan210905 <aryan2109shah@gmail.com>
2025-12-08 18:52:10 +0530

    second version of data prepared

--- DVC/main.py
+++ DVC/main.py
@@ -3,11 +3,11 @@ import os
 
 # Create a small dataset (example: student info)
 data = {
-    'ID': [1, 2, 3, 4, 5],
-    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-    'Age': [20, 21, 19, 22, 20],
-    'Branch': ['CSE', 'ECE', 'ME', 'CSE', 'IT'],
-    'Marks': [85, 78, 90, 88, 76]
+    'ID': [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10],
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Jenny', 'Quil', 'Sid', 'Freya', 'Henry'],
+    'Age': [20, 21, 19, 22, 20, 14, 39, 23, 21, 22],
+    'Branch': ['CSE', 'ECE', 'ME', 'CSE', 'IT', 'ME', 'CSE', 'EE', 'ME', 'ECE'],
+    'Marks': [85, 78, 90, 88, 76, 95, 35, 84, 55, 79]
 }
 
 # Convert to DataFrame
